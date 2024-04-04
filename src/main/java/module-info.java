b2767ae0dc3f56d6c9ab6884d432fc93128f200b
Jan Gluźniewicz <Jan.gluzniewicz@gmail.com>
2024-04-05 01:36:08 +0200

    module-info.java update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,6 @@
 module cryptography.tripledes {
     requires javafx.controls;
     requires javafx.fxml;
-
-
-    opens cryptography.tripledes to javafx.fxml;
-    exports cryptography.tripledes;
     exports cryptography.tripledes.gui;
     opens cryptography.tripledes.gui to javafx.fxml;
 }
