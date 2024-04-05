0cdcd5702b7190584ada4cbc26f815eb845120a7
Jan Gluźniewicz <Jan.gluzniewicz@gmail.com>
2024-04-05 16:48:17 +0200

    key8Transformation added

--- src/main/java/cryptography/tripledes/logic/Transformations.java
+++ src/main/java/cryptography/tripledes/logic/Transformations.java
@@ -1,4 +1,15 @@
 package cryptography.tripledes.logic;
 
 public class Transformations {
+    public byte[] key8Transformation(byte[] key) {
+        byte[] bits = new byte[56];
+        int index = 0;
+        for (int i = 0; i < 64; i++) {
+            if (i % 8 == 7) {
+                continue;
+            }
+            bits[index++] = key[i];
+        }
+        return bits;
+    }
 }
