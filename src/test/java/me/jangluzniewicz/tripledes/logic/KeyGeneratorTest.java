9dc9bc98931c61be5bd692ad0df6d2672bc86974
Jan Gluźniewicz <Jan.gluzniewicz@gmail.com>
2024-05-17 12:50:33 +0200

    tests added

--- src/test/java/me/jangluzniewicz/tripledes/logic/KeyGeneratorTest.java
+++ src/test/java/me/jangluzniewicz/tripledes/logic/KeyGeneratorTest.java
@@ -1,20 +1,16 @@
 package me.jangluzniewicz.tripledes.logic;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import java.util.BitSet;
 import static org.junit.jupiter.api.Assertions.*;
 
 class KeyGeneratorTest {
-    KeyGenerator keyGenerator;
-
-    @BeforeEach
-    void setUp() {
-        keyGenerator = new KeyGenerator();
-    }
-
     @Test
-    void generateKey() {
-
+    void testGenerateKey() {
+        KeyGenerator keyGenerator = new KeyGenerator();
+        BitSet key = keyGenerator.generateKey();
+        assertNotNull(key);
+        BitSet key2 = keyGenerator.generateKey();
+        assertNotEquals(key, key2);
     }
 }
